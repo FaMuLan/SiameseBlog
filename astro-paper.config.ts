1d31e2f85a3d21e762b9e6321c976e8e9965a132
FaMuLan <fa_mulan@163.com>
2026-07-16 18:49:01 +0800

    添加社交帐号，以及社交平台对应的图标

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -28,8 +28,10 @@ export default defineAstroPaperConfig({
     search: "pagefind",
   },
   socials: [
+    { name: "bilibili", url: "https://space.bilibili.com/30105172"},
+    { name: "weibo",    url: "https://weibo.com/u/1951359417"},
     { name: "github",   url: "https://github.com/FaMuLan/SiameseBlog" },
-    { name: "mail",     url: "mailto:fa_mulan@163.com" },
+    { name: "mail",     url: "mailto:fa_mulan@163.com" }
   ],
   shareLinks: [
     { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
