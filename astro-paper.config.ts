58c58ca3c96a2594a4ff55d66c3b6205829080aa
FaMuLan <fa_mulan@163.com>
2026-07-19 14:15:07 +0800

    增加itch.io平台的链接

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -28,8 +28,9 @@ export default defineAstroPaperConfig({
     search: "pagefind",
   },
   socials: [
-    { name: "bilibili", url: "https://space.bilibili.com/30105172"},
-    { name: "weibo",    url: "https://weibo.com/u/1951359417"},
+    { name: "bilibili", url: "https://space.bilibili.com/30105172" },
+    { name: "weibo",    url: "https://weibo.com/u/1951359417" },
+    { name: "itch.io",  url: "https://famulan.itch.io" },
     { name: "github",   url: "https://github.com/FaMuLan/SiameseBlog" },
     { name: "mail",     url: "mailto:fa_mulan@163.com" }
   ],
